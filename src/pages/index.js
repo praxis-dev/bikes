29d5cfa630467b88a58adefbf32525a9d8fff2a9
praxis <chesnokovprm@gmail.com>
2022-09-30 12:06:24 +0600

    primary_progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import "./styles.scss";
 
-console.log("test");
+let breakPoint = window.matchMedia("(max-width: 693px)");
+const primarySection = document.querySelector(".primary");
+const primaryTitle = document.querySelector(".primary__title");
+const primaryTextContainer = document.querySelector(".primary__text-container");
+const primaryIllustrationContainer = document.querySelector(
+  ".primary__illustration-container"
+);
+const primaryText = document.querySelector(".primary__text");
+
+function mobileTitleReposition() {
+  if (breakPoint.matches) {
+    console.log("reposition");
+    primaryTitle.after(primaryIllustrationContainer);
+  } else {
+    primaryTextContainer.after(primaryIllustrationContainer);
+  }
+}
+
+mobileTitleReposition();
+
+breakPoint.addEventListener("change", () => mobileTitleReposition());
