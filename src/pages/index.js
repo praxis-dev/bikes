a4483be1787457ff6686a43a95050fb832ebce43
praxis <chesnokovprm@gmail.com>
2022-10-02 11:05:39 +0600

    highway_progress_2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import "./styles.scss";
 
 let breakPoint = window.matchMedia("(max-width: 710px)");
-const primarySection = document.querySelector(".primary");
+
+// primaryTitleReposition
+
 const primaryTitle = document.querySelector(".primary__title");
 const primaryTextContainer = document.querySelector(".primary__text-container");
 const primaryIllustrationContainer = document.querySelector(
   ".primary__illustration-container"
 );
-const primaryText = document.querySelector(".primary__text");
 
 function mobileTitleReposition() {
   if (breakPoint.matches) {
@@ -21,3 +22,76 @@ function mobileTitleReposition() {
 mobileTitleReposition();
 
 breakPoint.addEventListener("change", () => mobileTitleReposition());
+
+// hideHighwayPicForMobile
+
+const highwayIllustrations = document.querySelectorAll(".highway__image");
+console.log(highwayIllustrations);
+
+function hideHighwayPicForMobile() {
+  if (breakPoint.matches) {
+    highwayIllustrations[1].style.display = "none";
+  } else {
+    highwayIllustrations[1].style.display = "block";
+  }
+}
+
+hideHighwayPicForMobile();
+
+breakPoint.addEventListener("change", () => hideHighwayPicForMobile());
+
+// repositionHighwaySelectorForMobile
+
+const highwaySection = document.querySelector(".highway");
+const highwayTitle = document.querySelector(".highway__title");
+const highwayPicSelector = document.querySelector(
+  ".highway__slider-buttons-container"
+);
+console.log(highwayPicSelector);
+const mobileTitleAndSelectorWrapper = document.createElement("div");
+const highwayMobileLine = document.querySelector(".highway__mobile-line");
+const highwayArrowLeftContainer = document.querySelector(
+  ".highway__slider-button-left"
+);
+const highwayArrowRightContainer = document.querySelector(
+  ".highway__slider-button-right"
+);
+
+function repositionHighwaySelectorForMobile() {
+  if (breakPoint.matches) {
+    mobileTitleAndSelectorWrapper.classList.add(
+      "highway__title-and-selector-wrapper"
+    );
+    highwayMobileLine.after(highwayTitle);
+    highwayTitle.after(mobileTitleAndSelectorWrapper);
+    mobileTitleAndSelectorWrapper.appendChild(highwayTitle);
+    mobileTitleAndSelectorWrapper.appendChild(highwayPicSelector);
+    mobileTitleAndSelectorWrapper.style.display = "flex";
+    mobileTitleAndSelectorWrapper.style.flexDirection = "row";
+    mobileTitleAndSelectorWrapper.style.justifyContent = "space-between";
+    mobileTitleAndSelectorWrapper.style.alignItems = "center";
+    mobileTitleAndSelectorWrapper.style.marginBottom = "22px";
+
+    highwayArrowLeftContainer.style.width = "24px";
+    highwayArrowLeftContainer.style.height = "24px";
+    highwayArrowRightContainer.style.width = "24px";
+    highwayArrowRightContainer.style.height = "24px";
+  } else {
+    highwayArrowLeftContainer.style.width = "50px";
+    highwayArrowLeftContainer.style.height = "50px";
+    highwayArrowRightContainer.style.width = "50px";
+    highwayArrowRightContainer.style.height = "50px";
+
+    highwaySection.prepend(highwayTitle);
+
+    mobileTitleAndSelectorWrapper.remove();
+
+    highwaySection.appendChild(highwayPicSelector);
+  }
+}
+
+repositionHighwaySelectorForMobile();
+
+breakPoint.addEventListener("change", () =>
+  repositionHighwaySelectorForMobile()
+);
