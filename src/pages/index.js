cd8cf63a42268ca8a809258dbf815b343edb6397
praxis <chesnokovprm@gmail.com>
2022-11-03 08:39:20 +0600

    highway_complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -104,6 +104,33 @@ const highwayImagesLeftContainer = document.querySelector(
 );
 const highwayTextContainer = document.querySelector(".highway__text-container");
 
+// reposition highway selector
+
+function repositionHighwaySelectorForMobile() {
+  if (breakPoint.matches) {
+    highwayPicSelector.style.position = "absolute";
+    highwayPicSelector.style.top = "15%";
+    highwayPicSelector.style.right = "10%";
+    highwayArrowLeftContainer.style.width = "24px";
+    highwayArrowLeftContainer.style.height = "24px";
+    highwayArrowRightContainer.style.width = "24px";
+    highwayArrowRightContainer.style.height = "24px";
+  } else {
+    highwayArrowLeftContainer.style.width = "50px";
+    highwayArrowLeftContainer.style.height = "50px";
+    highwayArrowRightContainer.style.width = "50px";
+    highwayArrowRightContainer.style.height = "50px";
+    highwayPicSelector.style.position = "static";
+    highwaySection.appendChild(highwayPicSelector);
+  }
+}
+
+repositionHighwaySelectorForMobile();
+
+breakPoint.addEventListener("change", () =>
+  repositionHighwaySelectorForMobile()
+);
+
 // hideBikesCardsForMobile;
 
 const bikesCards = document.querySelectorAll(".bikes__card");
